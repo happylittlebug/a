570aa9666da4eb83a51b574da0fc0bf68feb7b20
happylittlebug <56034287+happylittlebug@users.noreply.github.com>
2019-10-09 19:21:35 +0900

    Add files via upload

---
+++ basic calculator.py
@@ -0,0 +1,18 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+result = int(num1) + int(num2)
+print("result: " + str(result))
+
+
+num3 = input("Enter a number: ")
+num4 = input("Enter another number: ")
+
+result2 = float(num3) + float(num4)
+print("result2: " + str(result2))
+
+num5 = input("Enter a number: ")
+num6 = input("Enter another number: ")
+
+result3 = pow(float(num5), float(num6))
+print("result3: " + str(result3))
